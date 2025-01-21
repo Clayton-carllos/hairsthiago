2e769f481847883afbd89d8c63b836f9c962e07e
Clayton-carllos <clayton.carllos@gmail.com>
2025-01-21 17:34:34 -0300

    ajuste email

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def agendar():
     try:
         msg_admin = Message(
             subject='Novo Agendamento Realizado',
-            recipients=['claytinhogames@gmail.com'],  # Substitua pelo e-mail do admin
+            recipients=['thiagohsj02@gmail.com'],  # Substitua pelo e-mail do admin
             body=f"Um novo agendamento foi feito:\n\n"
                  f"Nome: {nome}\n"
                  f"Telefone: {telefone}\n"
